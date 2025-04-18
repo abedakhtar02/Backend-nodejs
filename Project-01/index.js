4c51352a8d988c0423c0d50c81d031784b5ce2e8
Abed Akhtar <abedakhtar02@gmail.com>
2025-04-19 01:19:09 +0530

    REST API

---
+++ Project-01/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs = require("fs");
+
+const app = express();
+const PORT = 8080;
+
+//Middleware
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+    ${users.map((users) => `<li>${users.first_name}</li>`).join("")}
+    </ul>
+    `;
+  res.send(html);
+});
+
+//REST API
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+
+  .patch((req, res) => {
+    return res.json({ status: "Pending" });
+  })
+
+  .delete((req, res) => {
+    return res.json({ status: "Pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success", id: users.length + 1 });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server Started on port 8080! http://localhost:8080");
+});
